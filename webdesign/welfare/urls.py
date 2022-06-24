8c053ac94db912015f479b0126ee9b0afc4913e5
Edwin574 <owinoedwin574@gmail.com>
2022-06-24 14:38:06 +0300

    deleted lists folder

--- webdesign/welfare/urls.py
+++ webdesign/welfare/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path("", views.home, name="home"),
-    path("", views.lists, name="lists")
+    path("home", views.lists, name="lists")
 ]
--- webdesign/welfare/views.py
+++ webdesign/welfare/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def  home(request):
     return render(request,"home/homepage.html")
-
 def lists(request):
-    return render(request,"lists/lists.html")
+    return render(request,"home/lists.html")
